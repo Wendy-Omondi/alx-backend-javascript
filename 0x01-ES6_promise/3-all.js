a571d99ebe8482e7b68f30711816940a905efe22
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-07-18 14:25:21 +0000

    resolve promises and log to console

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,20 @@
+import { uploadPhoto, createUser } from './utils';
+
+
+export default function handleProfileSignup() {
+  let body
+  let firstName
+  let LastName
+
+  return uploadPhoto()
+    .then((data) => {
+      body = data.body;
+      createUser()
+        .then((data) => {
+          firstName = data.firstName;
+          lastName = data.lastName;
+          console.log(`${body} ${firstName} ${lastName}`);
+        })
+        .catch(() => new Error(console.log('Signup system offline')));
+    })
+}

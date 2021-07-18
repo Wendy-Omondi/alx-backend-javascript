655ca40de56a835d74edce09459cfdb0c0d5ad57
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-07-18 20:40:42 +0000

    throw error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+export default function guardrail(mathFunction) {
+  let queue = [];
+
+  try {
+    /* mathfunction calue appended to queue array */
+    queue.push(mathFunction());
+  } catch (e) {
+    /* Error message appended to queue with specific Number object */
+    queue.push(e.toString());
+  } finally {
+    /* message added to queue in every case */
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}

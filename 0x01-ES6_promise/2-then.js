e1411dc10bdb3ae34779972574022b3af8ec1dda
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-07-18 14:07:25 +0000

    finally catch promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  let p = new Promise((resolve, reject) => {
-    if(success) {
-      resolve({ status: 200, body: 'Success' })
-    } else {
-      reject(new Error())
-    }
-});
-
-return promise.then(() => {
-  console.log('Got a response from the API' + promise)
-})
-
+  return promise.then(() => (
+    { status: 200, body: 'success' }))
+    .catch(() => new Error ())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
-

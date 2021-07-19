eb83922268b3e3ba5d7b3c03d59381aa3da1a458
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-07-19 20:33:49 +0000

    hoisting

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -16,7 +16,7 @@ class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-class StudentHolberton {
+export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;

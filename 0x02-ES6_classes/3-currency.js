e3160bee35bbdf1a57ab55e2f6c18a48990017a0
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-07-19 19:50:48 +0000

    Methods, static methods, computed methods names

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,25 @@
+class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+  
+  set code(newCode) {
+    this._name = newCode;
+  }
+  get code() {
+    return this._code;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return (`${this.name} (${this.code})`);
+  }
+}
+export default Currency;
